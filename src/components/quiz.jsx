762090961bc9f4939bc76dd8e8b6cc6d29a859fa
Fahim  Abrar <138319217+iamfardinn@users.noreply.github.com>
2025-05-22 04:55:21 +0600

    Userstates updated

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,4 +1,57 @@
+import React, { useState } from "react";
+
 function Quiz() {
-    return <div>Quiz</div>
+  const questionBank =[
+    {
+        question: "What is the capital of France?",
+        options: ["Paris", "London", "Berlin", "Madrid"],
+        answer: "Paris"
+    },
+    
+    {
+        question: "what is the largest planet in our solar system?",
+        options: ["Earth", "Mars", "Jupiter", "Saturn"],
+        answer: "Jupiter"
+    },
+
+    {
+        question: "What does HTML stand for?",
+        options: ["Hyper Text Markup Language", "High Text Markup Language", "Hyper Tabular Markup Language", "None of these"],
+        answer: "Hyper Text Markup Language"
+    }
+  ];
+
+
+
+    const [optionSelected, setOptionSelected] = useState(null);
+
+
+  function handleSelectOption(option) {
+    
+setOptionSelected(option);
+
+  }
+
+    return (
+    
+    <div>
+        <h2>Question 1</h2>
+        <p className="question">{questionBank[0].question}</p>
+
+
+{questionBank[0].options.map((option) => (
+ <button className="option" onClick={() => handleSelectOption(option)}>
+    {option}
+    </button>
+))}
+
+<p>option Selected:{optionSelected}</p>
+
+<div className="nav-buttons">
+    <button>Previous</button>
+    <button>Next</button>
+</div>
+        </div>
+    )
 }
 export default Quiz
